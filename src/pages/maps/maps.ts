4e6ccba36bea0ce35d3402b83c0a748064024ab9
Mile <Mile@DESKTOP-K72BU75>
2018-04-19 23:35:00 +0100

    maps1

--- src/pages/maps/maps.ts
+++ src/pages/maps/maps.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import {IonicPage, MenuController, NavController, NavParams} from 'ionic-angular';
+import { LaunchNavigator, LaunchNavigatorOptions } from '@ionic-native/launch-navigator';
+import { Component,ViewChild,ElementRef } from '@angular/core';
+import { IonicPage, MenuController, NavController, NavParams, Platform } from 'ionic-angular';
 
 /**
  * Generated class for the MapsPage page.
@@ -7,20 +8,30 @@ import {IonicPage, MenuController, NavController, NavParams} from 'ionic-angular
  * See https://ionicframework.com/docs/components/#navigation for more info on
  * Ionic pages and navigation.
  */
-
+declare var google: any;
 @IonicPage()
 @Component({
   selector: 'page-maps',
   templateUrl: 'maps.html',
 })
 export class MapsPage {
-
-  apikey: 'AIzaSyClom9swe9VuLwuwKHfSyOnqSPRfWnCVt0';
-  constructor(public navCtrl: NavController, public navParams: NavParams, public menuCtrl: MenuController) {
+  @ViewChild('map') mapElement: ElementRef;
+  map: any;
+  constructor(public navCtrl: NavController, public navParams: NavParams, public menuCtrl: MenuController,
+  public platform: Platform) {
+    platform.ready().then(() =>{
+      this.initMap();
+    })
   }
-
+  initMap()
+  {
+    this.map = new google.maps.Map(this.mapElement.nativeElement,{
+      zoom: 7,
+      center: {lat: 41.85,lng:-87.65}
+    });
+  }
   ionViewDidLoad() {
-      this.menuCtrl.enable(false,"sidemenu");
+      this.menuCtrl.enable(true,"sidemenu");
       console.log('ionViewDidLoad MapsPage');
   }
 
